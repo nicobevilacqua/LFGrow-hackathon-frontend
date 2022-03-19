cf58ca6487bb05e72edde4ba41821b79399818e5
Nicolas Bevilacqua <nicobevilacqua@gmail.com>
2022-03-19 14:56:59 -0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+let base = '/';
+if (isProd) {
+  base = '/hackathon/';
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base,
+  plugins: [react()],
+});
