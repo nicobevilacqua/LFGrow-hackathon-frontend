8b67a90430ba03d33e0dc21b25b87702a1f50d6b
Nicolas Bevilacqua <nicobevilacqua@gmail.com>
2022-03-24 00:26:53 -0300

    svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
+
 import path from 'path';
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -13,7 +15,15 @@ if (isProd) {
 // https://vitejs.dev/config/
 export default defineConfig({
   base,
-  plugins: [react()],
+  plugins: [
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
