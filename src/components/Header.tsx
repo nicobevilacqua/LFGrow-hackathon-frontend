1eb26fb65b3171cef8062c5b4a5634648383636f
juancito.eth <juancitoeth@gmail.com>
2022-03-25 19:19:07 -0300

    add lens login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { signInWallet } from '../scripts/ethers-service';
+import { login } from '../scripts/login-users';
 
 const Header: React.FC = () => {
+  const handleLogin = async () => {
+    await login();
+  };
+
   return (
     <header className="flex justify-center align-center py-10">
       <div className="container">header</div>
+      <div className="container" onClick={handleLogin}>
+        Login Lens
+      </div>
     </header>
   );
 };
