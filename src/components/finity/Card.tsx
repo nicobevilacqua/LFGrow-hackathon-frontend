9ca7089a699c5f2c7c8c4f1b19c0e41d522d65ea
juancito.eth <juancitoeth@gmail.com>
2022-03-24 22:32:47 -0300

    abstract attributes

--- src/components/finity/Card.tsx
+++ src/components/finity/Card.tsx
@@ -1,32 +1,30 @@
 import React from 'react';
 import './Card.css';
 
-export default function Card() {
+export default function Card(props: {
+  title: string;
+  subtitle: string;
+  body: string;
+  thumbnail: string;
+  avatar: string;
+}) {
   return (
     <div className="component-86 flex-col-hcenter-vcenter">
-      <img
-        src="https://i.ytimg.com/vi/LG9uYoxFxtw/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB8WO6VLLV613E1diHNpyLfuDLRWw"
-        alt="Not Found"
-        className="image-39"
-      />
+      <img src={props.thumbnail} alt="Not Found" className="image-39" />
       <div className="frame-10425 flex-col-hcenter-vstart">
         <div className="root-card-heading-1 flex-col-hstart-vstart">
           <div className="root-card-heading flex-row-vcenter-hcenter">
             <div className="frame-10493 flex-col-hstart-vstart">
               <div className="frame-10435 flex-row-vstart-hstart">
                 <div className="frame-10434 flex-row-vstart-hstart">
-                  <img
-                    src="https://pbs.twimg.com/profile_images/1505032322717941761/EaFzE2yN_400x400.jpg"
-                    alt="Not Found"
-                    className="frame-10423"
-                  />
+                  <img src={props.avatar} alt="Not Found" className="frame-10423" />
                   <div className="frame-10425 flex-col-hcenter-vstart">
                     <div className="frame-10424 flex-col-hstart-vstart">
                       <div className="card-element-line-item-heading-enabled flex-col-hstart-vstart">
-                        <p className="txt-695">Lens Protocol</p>
+                        <p className="txt-695">{props.title}</p>
                       </div>
                       <div className="card-element-line-item-heading-enabled flex-col-hstart-vstart">
-                        <p className="txt-605">By Snoop Dogg</p>
+                        <p className="txt-605">{props.subtitle}</p>
                       </div>
                     </div>
                   </div>
@@ -34,10 +32,7 @@ export default function Card() {
               </div>
             </div>
           </div>
-          <p className="txt-771">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mus ut ornare felis pharetra,
-            eu in a tortor.
-          </p>
+          <p className="txt-771">{props.body}</p>
         </div>
       </div>
     </div>
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,16 +1,23 @@
 import Card from '@components/finity/Card';
 import React from 'react';
 
+const cardData = {
+  title: 'Lens Protocol',
+  subtitle: 'By Snoop Dogg',
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mus ut ornare felis pharetra, eu in a tortor.',
+  thumbnail: 'https://i.ytimg.com/vi/LG9uYoxFxtw/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB8WO6VLLV613E1diHNpyLfuDLRWw',
+  avatar: 'https://pbs.twimg.com/profile_images/1505032322717941761/EaFzE2yN_400x400.jpg',
+};
+
+const cards = Array(6).fill(cardData);
+
 const Feed = () => {
   return (
     <>
       <div style={{ display: 'flex', flexFlow: 'wrap', justifyContent: 'center', gap: 20 }}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {cards.map((card, index) => (
+          <Card key={index} {...card} />
+        ))}
       </div>
     </>
   );
