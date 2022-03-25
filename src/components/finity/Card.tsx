113407aac8add11255b640e94a74d961e339e8ac
juancito.eth <juancitoeth@gmail.com>
2022-03-24 22:41:35 -0300

    add link to video

--- src/components/finity/Card.tsx
+++ src/components/finity/Card.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Card.css';
+import { Link } from 'react-router-dom';
 
 export default function Card(props: {
   title: string;
@@ -7,34 +8,37 @@ export default function Card(props: {
   body: string;
   thumbnail: string;
   avatar: string;
+  cid: string;
 }) {
   return (
-    <div className="component-86 flex-col-hcenter-vcenter">
-      <img src={props.thumbnail} alt="Not Found" className="image-39" />
-      <div className="frame-10425 flex-col-hcenter-vstart">
-        <div className="root-card-heading-1 flex-col-hstart-vstart">
-          <div className="root-card-heading flex-row-vcenter-hcenter">
-            <div className="frame-10493 flex-col-hstart-vstart">
-              <div className="frame-10435 flex-row-vstart-hstart">
-                <div className="frame-10434 flex-row-vstart-hstart">
-                  <img src={props.avatar} alt="Not Found" className="frame-10423" />
-                  <div className="frame-10425 flex-col-hcenter-vstart">
-                    <div className="frame-10424 flex-col-hstart-vstart">
-                      <div className="card-element-line-item-heading-enabled flex-col-hstart-vstart">
-                        <p className="txt-695">{props.title}</p>
-                      </div>
-                      <div className="card-element-line-item-heading-enabled flex-col-hstart-vstart">
-                        <p className="txt-605">{props.subtitle}</p>
+    <Link to={`/video/${props.cid}`}>
+      <div className="component-86 flex-col-hcenter-vcenter">
+        <img src={props.thumbnail} alt="Not Found" className="image-39" />
+        <div className="frame-10425 flex-col-hcenter-vstart">
+          <div className="root-card-heading-1 flex-col-hstart-vstart">
+            <div className="root-card-heading flex-row-vcenter-hcenter">
+              <div className="frame-10493 flex-col-hstart-vstart">
+                <div className="frame-10435 flex-row-vstart-hstart">
+                  <div className="frame-10434 flex-row-vstart-hstart">
+                    <img src={props.avatar} alt="Not Found" className="frame-10423" />
+                    <div className="frame-10425 flex-col-hcenter-vstart">
+                      <div className="frame-10424 flex-col-hstart-vstart">
+                        <div className="card-element-line-item-heading-enabled flex-col-hstart-vstart">
+                          <p className="txt-695">{props.title}</p>
+                        </div>
+                        <div className="card-element-line-item-heading-enabled flex-col-hstart-vstart">
+                          <p className="txt-605">{props.subtitle}</p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
+            <p className="txt-771">{props.body}</p>
           </div>
-          <p className="txt-771">{props.body}</p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -7,6 +7,7 @@ const cardData = {
   body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mus ut ornare felis pharetra, eu in a tortor.',
   thumbnail: 'https://i.ytimg.com/vi/LG9uYoxFxtw/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB8WO6VLLV613E1diHNpyLfuDLRWw',
   avatar: 'https://pbs.twimg.com/profile_images/1505032322717941761/EaFzE2yN_400x400.jpg',
+  cid: 'QmXavKrZujmkFm4PHJ1fnEUWHoH5Uq5QfXFQEBeXhFeGkE',
 };
 
 const cards = Array(6).fill(cardData);
